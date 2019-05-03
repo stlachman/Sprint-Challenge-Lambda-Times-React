034183f08817afdd486ed4bb1b806c47a990c792
Seth Lachman <lachmanseth17@gmail.com>
2019-05-03 13:18:36 -0400

    Added alt tag to image. Updated proptype on cards component. Started working on carousel component.

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
+
 // Complete this Carousel 
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      images: [],
+      index: 0
     }
   }
   componentDidMount(){
-
+    this.setState({ images: carouselData})
   }
 
   leftClick = () => {
-
+    
   }
 
   rightClick = () => {
@@ -21,12 +23,15 @@ export default class Carousel extends Component {
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.images[this.state.index]} style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
+        {this.state.images.map(image => {
+          return <img src={image}/>
+        })}
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Card from './Card';
 
-const Cards = props => {
-  console.log(props);
-  return (
-    <div className="cards-container">
-      {props.cards.map((card, i) => {
-        return <Card card={card} key={i} />
-      })}
-    </div>
-  )
-}
+const Cards = (props) => {
+	return (
+		<div className="cards-container">
+			{props.cards.map((card, i) => {
+				return <Card card={card} key={i} />;
+			})}
+		</div>
+	);
+};
 
 // Make sure you include prop types for all of your incoming props
 Cards.propTypes = {
-  cards: PropTypes.arrayOf(PropTypes.object)
+	cards: PropTypes.arrayOf(
+		PropTypes.shape({
+			headline: PropTypes.string,
+			tab: PropTypes.string,
+			img: PropTypes.string,
+			author: PropTypes.string
+		})
+	)
 };
 
 export default Cards;
