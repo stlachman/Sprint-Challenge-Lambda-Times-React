21f75254962e744794e67619f4886f98a3b6ad80
Seth Lachman <lachmanseth17@gmail.com>
2019-05-03 13:43:50 -0400

    Added infinte scroll to right and left for carousel component.

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,43 +1,55 @@
 import React, { Component } from 'react';
-import { carouselData } from '../../data'
+import { carouselData } from '../../data';
 
-// Complete this Carousel 
+// Complete this Carousel
 export default class Carousel extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      images: [],
-      index: 0
-    }
-  }
-  componentDidMount(){
-    this.setState({ images: carouselData})
-    this.selectedImage();
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			images: [],
+			index: 0
+		};
+	}
+	componentDidMount() {
+		this.setState({ images: carouselData });
+		this.selectedImage();
+	}
 
-  leftClick = () => {
-    this.setState({
-      index: this.state.index - 1
-    })
-  }
+	leftClick = () => {
+		if (this.state.index === 0) {
+			this.setState({ index: this.state.images.length - 1 });
+		} else {
+			this.setState({
+				index: this.state.index - 1
+			});
+		}
+	};
 
-  rightClick = () => {
-    this.setState({
-      index: this.state.index + 1
-    })
-  }
+	rightClick = () => {
+		if (this.state.index < this.state.images.length - 1) {
+			this.setState({
+				index: this.state.index + 1
+			});
+		} else {
+			this.setState({ index: 0 });
+		}
+	};
 
-  selectedImage = () => {
-    return <img src={this.state.images[this.state.index]} style={{display: 'block'}} />
-  }
-  
-  render(){
-    return (
-      <div className="carousel">
-        {this.selectedImage()}
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
-      </div>
-    )
-  }
+	selectedImage = () => {
+		return <img src={this.state.images[this.state.index]} style={{ display: 'block' }} />;
+	};
+
+	render() {
+		return (
+			<div className="carousel">
+				{this.selectedImage()}
+				<div className="left-button" onClick={this.leftClick}>
+					{'<'}
+				</div>
+				<div className="right-button" onClick={this.rightClick}>
+					{'>'}
+				</div>
+			</div>
+		);
+	}
 }
